67423d7d2e34acde145d82804354f52123b870c8
gablou <el.gablou@outlook.fr>
2017-09-02 23:28:14 +0200

    stuf to animate

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,12 @@
+import { expand } from 'rxjs/operator/expand';
 import { Component, OnInit } from '@angular/core';
+type State = 'collapsed'| 'expanded';
+
+interface Row {
+  state: State;
+  expanded: boolean;
+  loaded: boolean;
+}
 
 @Component({
   selector: 'app-list',
@@ -6,10 +14,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
-
+  list: Row[];
   constructor() { }
 
   ngOnInit() {
+    this.list = [
+      { state: 'collapsed', expanded: false, loaded: true },
+      { state: 'collapsed', expanded: false, loaded: true },
+      { state: 'collapsed', expanded: false, loaded: true },
+      { state: 'collapsed', expanded: false, loaded: true },
+      { state: 'collapsed', expanded: false, loaded: true },
+      { state: 'collapsed', expanded: false, loaded: true },
+      { state: 'collapsed', expanded: false, loaded: true },
+      { state: 'collapsed', expanded: false, loaded: true },
+      { state: 'collapsed', expanded: false, loaded: true },
+      { state: 'collapsed', expanded: false, loaded: true },
+      { state: 'collapsed', expanded: false, loaded: true }
+    ];
+  }
+
+  expand (row: Row): void {
+    row.expanded = true;
+    row.state = 'expanded';
+  }
+
+  collapse (row: Row): void {
+    row.expanded = false;
+    row.state = 'collapsed';
   }
 
 }
